a8352573e6e250ed15b8f4028fcef26af475a359
Dakshinesh-cmd <dakshineshprabhu@gmail.com>
2025-06-16 20:55:45 +0530

    print and input code

--- Basics.py
+++ Basics.py
@@ -1,3 +1,3 @@
 print("Welcome to Python basics!")
 name =input("What's your name? ")
-print(f"Hello , Dakshinesh! Glad you are learning Python.")
+print(f"Hello , {name}! Glad you are learning Python.")
