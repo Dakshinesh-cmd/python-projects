34b24935d81b48fbcaf1ea5f371e7711dde5220b
Dakshinesh P <dakshinehprabhu@gmail.com>
2025-06-17 17:35:29 +0530

    Added profile.py

---
+++ profile.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height in meters: "))
+
+future_age = age + 5
+
+print(f"Hi {name}, you are {age} years old and {height} meters tall.")
+print(f"In 5 years, you will be {future_age} years old.")
