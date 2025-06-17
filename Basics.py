24d8a09c851133e5a1b0b056914010fa5c14fd18
Dakshinesh P <dakshinehprabhu@gmail.com>
2025-06-17 17:19:27 +0530

    Removed Basics.py

--- Basics.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to Python basics!")
-name =input("What's your name? ")
-print(f"Hello , {name}! Glad you are learning Python.")
-
-print("Hello, Daksh! Glad you're learning Python.")
-
-
