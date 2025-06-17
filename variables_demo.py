1442caf8cedc9a4c35e7b45b854366bcc9a239c4
Dakshinesh P <dakshinehprabhu@gmail.com>
2025-06-17 17:20:33 +0530

    Removed variables_demo.py

--- variables_demo.py
+++
@@ -1,9 +0,0 @@
-
-name = input("Enter your name: ")
-age =  input("Enter your age: ") 
-print("Hello!,", name)
-
-name = input("Enter your name: ")
-age =  input("Enter your age: ") 
-print("Hello!,", name)
-print("Your age is", age)
