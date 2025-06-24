bffdb2ed26beee19e3b9e3531ab15e772e110853
Dakshinesh P <dakshinehprabhu@gmail.com>
2025-06-24 16:58:52 +0530

    Updated functions.py with new examples

--- functions.py
+++ functions.py
@@ -36,3 +36,32 @@ def add_numbers(*args):
  return total
 print(add_numbers(5 ,10))
 print(add_numbers(1, 2, 3, 4, 5))
+
+def print_info(**kwargs):
+ for key, value in kwargs.items():
+  print(f"{key}: {value}")
+print_info(name="Dakshi", age=22, role="learner")
+
+add = lambda a, b: a + b
+output = add(1, 6)
+print(output)
+
+number = [2 , 4 , 6 , 8]
+squared = list(map(lambda x: x*x, number))
+print(squared)
+
+name = ["Alice", "Bob", "Ankit", "John", "Arya"]
+a = list(filter(lambda name : name.startswith("A"), name))
+print(a)
+
+people = [("Alice", 25), ("Bob", 20), ("Charlie", 30)]
+arrange = sorted(people, key=lambda person: person[1])
+print(arrange)
+
+name = ["Alice", "Bob", "Charlie"]
+age = [25, 30, 18]
+combined= list(zip(name, age))
+print(combined)
+
+for name, age in zip(name , age):
+ print(f"{name} is {age} years old")
