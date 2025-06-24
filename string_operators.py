a64137ecc7a85e6be5bcd75a761075504cccad2b
Dakshinesh P <dakshinehprabhu@gmail.com>
2025-06-24 18:18:34 +0530

    Added string_operators.py with split, join, and slicing examples

---
+++ string_operators.py
@@ -0,0 +1,29 @@
+sentence = "Python is fun"
+word = sentence.split()
+print(word)
+
+joined = '-'.join(word)
+print(joined)
+
+sentence = "Learning Python is really intersting."
+word = sentence.split()
+print(word)
+
+join = '_'.join(word)
+print(join)
+
+text = "HelloWorld"
+print(text[0:5])
+print(text[5:10])
+
+text = "Programming"
+print(text[0:6])
+print(text[3:])
+print(text[ :4])
+print(text[3:7])
+
+word = "Development"
+print(word[2:9])
+
+msg = "Learning Python"
+print(msg[-6:])
